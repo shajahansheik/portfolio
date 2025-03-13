a8efa95cc1c7b20a02de7e4763a904984618411b
Shajahan Shaik <87851422+SHAJAHAN-OHT@users.noreply.github.com>
2025-03-14 00:59:51 +0530

    started projects page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function page() {
               Get in touch
             </h2>
             <p className="mt-6 text-base text-gray-600">
-            Have a project in mind or just want to connect? Feel free to reach out! I'm always open to discussing new opportunities, collaborations, or just chatting about tech. Let's build something amazing together!
+              Have a project in mind or just want to connect? Feel free to reach out! I'm always open to discussing new opportunities, collaborations, or just chatting about tech. Let's build something amazing together!
             </p>
             <dl className="mt-10 space-y-4 text-base/7 text-gray-600">
               <div className="flex gap-x-4">
@@ -44,94 +44,94 @@ export default function page() {
                 </dt>
                 <dd>
                   <a href="mailto:shajahan.j2se@gmail.com" className="hover:text-gray-900">
-                  shajahan.j2se@gmail.com
+                    shajahan.j2se@gmail.com
                   </a>
                 </dd>
               </div>
             </dl>
           </div>
-            <form className='p-20'>
-                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  <div>
-                    <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
-                      First name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="first-name"
-                        name="first-name"
-                        type="text"
-                        autoComplete="given-name"
-                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline  -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
-                      Last name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="last-name"
-                        name="last-name"
-                        type="text"
-                        autoComplete="family-name"
-                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
-                      Email
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
-                      Phone number
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="phone-number"
-                        name="phone-number"
-                        type="tel"
-                        autoComplete="tel"
-                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
-                      Message
-                    </label>
-                    <div className="mt-2.5">
-                      <textarea
-                        id="message"
-                        name="message"
-                        rows={4}
-                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                        defaultValue={''}
-                      />
-                    </div>
-                  </div>
+          <form className='p-20'>
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="first-name"
+                    name="first-name"
+                    type="text"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline  -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
                 </div>
-                <div className="mt-8 flex justify-end">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Send message
-                  </button>
               </div>
-            </form>
+              <div>
+                <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="last-name"
+                    name="last-name"
+                    type="text"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
+                  Phone number
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="phone-number"
+                    name="phone-number"
+                    type="tel"
+                    autoComplete="tel"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    defaultValue={''}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 flex justify-end">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Send message
+              </button>
+            </div>
+          </form>
         </div>
       </div>
       <div className="h-[12%]">
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import BottomNav from '../components/bottomnav'
 
 export default function page() {
   return (
-    <div>Projects</div>
+    <div className='space-y-5'>
+      <div className='px-60 text-3xl'>Projects</div>
+      <div className='overflow-scroll h-[41rem] py-5 space-y-4'>
+        <div className='space-y-2  pb-8'>
+          <div className='px-64 text-lg'>EMR</div>
+          <div className='px-64 indent-10'>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate voluptatibus deserunt adipisci earum quibusdam saepe sapiente nostrum nihil ad molestiae excepturi nemo error fugiat ipsa deleniti, modi ea nesciunt. Error?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quas quo nam quae dignissimos officia illo dolorem ipsum dolor iusto. Facilis, odio illum nemo explicabo ullam esse quis fugiat reiciendis.</p>
+          </div>
+        </div>
+        <div className='mx-16 text-gray-300'><hr /></div>
+        <div className='space-y-2  pb-8'>
+          <div className='px-64 text-lg'>Smart Reports</div>
+          <div className='px-64 indent-10'>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate voluptatibus deserunt adipisci earum quibusdam saepe sapiente nostrum nihil ad molestiae excepturi nemo error fugiat ipsa deleniti, modi ea nesciunt. Error?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quas quo nam quae dignissimos officia illo dolorem ipsum dolor iusto. Facilis, odio illum nemo explicabo ullam esse quis fugiat reiciendis.</p>
+          </div>
+        </div>
+        <BottomNav />
+      </div>
+    </div>
   )
 }
