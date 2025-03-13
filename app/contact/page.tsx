d37bbf525f1e18df8ff19ce357ff825cd3d0f39c
Shajahan Shaik <87851422+SHAJAHAN-OHT@users.noreply.github.com>
2025-03-13 10:48:52 +0530

    contact page created

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,142 @@
 import React from 'react'
 import BottomNav from '../components/bottomnav'
+import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 export default function page() {
   return (
     <div className="h-full">
       <div className="h-[88%]">
-      <div></div>
-      <div></div>
+        <div className='grid grid-cols-2 gap-10 mx-10 h-full'>
+          <div className='p-20'>
+            <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+              Get in touch
+            </h2>
+            <p className="mt-6 text-base text-gray-600">
+            Have a project in mind or just want to connect? Feel free to reach out! I'm always open to discussing new opportunities, collaborations, or just chatting about tech. Let's build something amazing together!
+            </p>
+            <dl className="mt-10 space-y-4 text-base/7 text-gray-600">
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Address</span>
+                  <BuildingOffice2Icon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                </dt>
+                <dd>
+                  Hyderabad
+                  <br />
+                  Telangana
+                </dd>
+              </div>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Telephone</span>
+                  <PhoneIcon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                </dt>
+                <dd>
+                  <a href="tel:+91 9000425100" className="hover:text-gray-900">
+                    +91 8639445655
+                  </a>
+                </dd>
+              </div>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Email</span>
+                  <EnvelopeIcon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                </dt>
+                <dd>
+                  <a href="mailto:shajahan.j2se@gmail.com" className="hover:text-gray-900">
+                  shajahan.j2se@gmail.com
+                  </a>
+                </dd>
+              </div>
+            </dl>
+          </div>
+            <form className='p-20'>
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                  <div>
+                    <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+                      First name
+                    </label>
+                    <div className="mt-2.5">
+                      <input
+                        id="first-name"
+                        name="first-name"
+                        type="text"
+                        autoComplete="given-name"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline  -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
+                      Last name
+                    </label>
+                    <div className="mt-2.5">
+                      <input
+                        id="last-name"
+                        name="last-name"
+                        type="text"
+                        autoComplete="family-name"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+                      Email
+                    </label>
+                    <div className="mt-2.5">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
+                      Phone number
+                    </label>
+                    <div className="mt-2.5">
+                      <input
+                        id="phone-number"
+                        name="phone-number"
+                        type="tel"
+                        autoComplete="tel"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+                      Message
+                    </label>
+                    <div className="mt-2.5">
+                      <textarea
+                        id="message"
+                        name="message"
+                        rows={4}
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                        defaultValue={''}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-8 flex justify-end">
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Send message
+                  </button>
+              </div>
+            </form>
+        </div>
       </div>
       <div className="h-[12%]">
-        <BottomNav/>
-        </div>
+        <BottomNav />
+      </div>
     </div>
   )
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Experience from '../components/experience'
+import Education from '../components/education'
+import Skills from '../components/skills'
 
 export default function page() {
   return (
-    <div>Resume</div>
+    <div>
+      <div className='px-60 text-3xl'>Resume</div>
+      <div>
+        <Experience/>
+        <Education/>
+        <Skills/>
+      </div>
+    </div>
   )
 }
